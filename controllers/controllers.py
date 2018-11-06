77f89622e9dcb32324f31e2528697429a74c35e1
tamvm <tamvm@vng.com.vn>
2018-11-06 21:29:48 +0700

    add config jupyter

--- controllers/controllers.py
+++ controllers/controllers.py
@@ -1,10 +1,18 @@
 # -*- coding: utf-8 -*-
 from odoo import http
+import requests
 
 class IPython(http.Controller):
-    @http.route('/ipython-report/', auth='public')
-    def index(self, **kw):
-        return "Hello, world"
+    @http.route(['/ipython-report', '/ipython-report/<path:path>'], auth='public')
+    def index(self, *path, **kw):
+        print(kw)
+        print(path)
+        print(self)
+        print(http.request)
+        print(http.request.env)
+
+        r = requests.get('http://localhost:8888/notebooks/guardian_gaza.ipynb?token=e4fafee6f8b80c9a5d591d1b541b162ca78a021169070387')        
+        return r.text
 
 #     @http.route('/exper/exper/objects/', auth='public')
 #     def list(self, **kw):
